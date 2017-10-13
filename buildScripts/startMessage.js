083329033387448ced9d810df0d0ee7d22984522
ndepaul <nicolas.depaul@gmail.com>
2017-10-12 22:17:32 -0300

    Automation tasks using npm scripts in package.json!

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,5 @@
+const chalk = require('chalk');
+
+console.log(chalk.green('Starting app in dev mode...'));
+
+
---
+++ dist/buildScripts/startMessage.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var chalk = require('chalk');
+
+console.log(chalk.green('Starting app in dev mode...'));
+//# sourceMappingURL=startMessage.js.map
