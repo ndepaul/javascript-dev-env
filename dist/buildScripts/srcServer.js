10793733f3467a64ae5b8f743140da3f933bf532
ndepaul <nicolas.depaul@gmail.com>
2017-10-14 00:02:17 -0300

    Webpack implementation, webpackconfig.dev.js file added to configure webpack. index.js file added with a javascript file that we call from index.html. bundle.js bundle file.

--- dist/buildScripts/srcServer.js
+++ dist/buildScripts/srcServer.js
@@ -12,10 +12,24 @@ var _open = require('open');
 
 var _open2 = _interopRequireDefault(_open);
 
+var _webpack = require('webpack');
+
+var _webpack2 = _interopRequireDefault(_webpack);
+
+var _webpackconfig = require('../webpackconfig.dev');
+
+var _webpackconfig2 = _interopRequireDefault(_webpackconfig);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var port = 3000;
 var app = (0, _express2.default)();
+var compiler = (0, _webpack2.default)(_webpackconfig2.default);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: _webpackconfig2.default.output.publicPath
+}));
 
 app.get('/', function (req, res) {
   res.sendFile(_path2.default.join(__dirname, '../src/index.html'));
---
+++ dist/src/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var _numeral = require('numeral');
+
+var _numeral2 = _interopRequireDefault(_numeral);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var courseValue = (0, _numeral2.default)(1000).format('$0,0.00');
+console.log('I would pay ' + courseValue + ' for this awesome course!');
+//# sourceMappingURL=index.js.map
