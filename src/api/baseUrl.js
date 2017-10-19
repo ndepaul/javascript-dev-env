c659e0f941048a4488345cf10d257bcdc463f42d
ndepaul <nicolas.depaul@gmail.com>
2017-10-19 00:07:06 -0300

    Setting the production environment. Webpackconfig.prod.js, build.js and distServer.js. Minification configured and gzip compression. baseUrl for development it's the same + /?usemockapi=true

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,6 +1,17 @@
-// File to determine the right API web server to point depends on the environment
+/* File to determine the right API web server to point depends on the environment, to use
+   the mock api we should put in the browser /?usemockapi=true
+*/
+
+function getQueryStringParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g," "));
+}
 
 export default function getBaseUrl() {
-  const inDevelopment = window.location.hostname === 'localhost';
-  return inDevelopment ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
 }
